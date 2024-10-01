4e28c7ba801e8c6a313398ad758a0a96b8653432
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-10-01 14:56:06 +0530

    Add files via upload
    My first python

---
+++ Derik james.py
@@ -0,0 +1,8 @@
+print("Hello Python")
+name=input("enter your name :")
+print("Name of the student",name)
+roll_number=int(input("your roll number :"))
+print("Roll Number of the student",roll_number+1)
+cgpa=float(input("enter your cgpa"))
+perntage_marks=cgpa*10
+print("cgpa",perce)
