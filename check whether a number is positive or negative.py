ac604430f123d7c452a604b11ca52ad5eda1e05f
derikjames636 <derikjames2028@cs.sjcetpalai.ac.in>
2024-10-15 14:09:42 +0530

    Add files via upload

---
+++ check whether a number is positive or negative.py
@@ -0,0 +1,8 @@
+'''check whether a numaber is positive or not'''
+#Date: 15-10-2024
+number=int(input('Enter a number:'))
+if number>0:
+    print('The given number :',number,'is positive')
+else :
+    print('The given number',number,'is negative')
+
